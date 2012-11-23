d85762469e2808ba093bf0cebe0573579d8e3211
Gunhee Lee <teuksu00@gmail.com>
2012-11-23 15:46:44 +0900

    * View.java -> Very simple GUI window with swing

--- view/View.java
+++ view/View.java
@@ -6,6 +6,23 @@ import javax.swing.*;
 public class View
 {
     private static void createAndShowGUI() {
+        JFrame frame = new JFrame("simEvo - Prototype");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JLabel label = new JLabel("simEvo test");
+        frame.getContentPane().add(label);
+
+        // Display the window
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() 
+        {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
